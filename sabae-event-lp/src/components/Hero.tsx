536f079d2e778c760cc6b699fe9cc425bafb11d7
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-06-22 15:07:01 +0900

    SP版のMV画像をインポート

--- sabae-event-lp/src/components/Hero.tsx
+++ sabae-event-lp/src/components/Hero.tsx
@@ -5,6 +5,7 @@ export const Hero = () => {
 		<>
 			<SECTION_HeroContainer id="1">
 				<IMG_Image src="images/MV.png" alt="フェスのメイン画像" />
+				<IMG_Image_SP src="images/MV_SP.png" alt="フェスのメイン画像" />
 			</SECTION_HeroContainer>
 		</>
 	);
@@ -16,4 +17,17 @@ const SECTION_HeroContainer = styled.section`
 
 const IMG_Image = styled.img`
 	width: 100%;
+
+	@media (max-width: 800px) {
+		display: none;
+	}
+`;
+
+const IMG_Image_SP = styled.img`
+	display: none;
+
+	@media (max-width: 800px) {
+		display: block;
+		width: 100%;
+	}
 `;
