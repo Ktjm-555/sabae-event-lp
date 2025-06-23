9ee4bc8037041952ef3ee9445ea7148b94a7f619
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-06-24 00:20:36 +0900

    Updates

--- assets/index-B6mdmDgB.js
+++ assets/index-B6mdmDgB.js
@@ -198,7 +198,7 @@ Error generating stack: `+l.message+`
 
 	span > span:nth-child(2) {
 		display: inline-block;
-		vertical-align: -0.1em;
+		vertical-align: -7px;
 		font-size: 85px;
 		font-weight: 400;
 		line-height: 0;
