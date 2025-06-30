55ba84a2c0fd89ca571837f3b7e2f25126c42a1e
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-06-30 23:20:32 +0900

    バージョン表示

--- sabae-event-lp/vite.config.ts
+++ sabae-event-lp/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pkg from "./package.json" assert { type: "json" };
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/sabae-event-lp/",
 	plugins: [react()],
+	define: {
+		"import.meta.env.VITE_APP_VERSION": JSON.stringify(pkg.version),
+	},
 });
