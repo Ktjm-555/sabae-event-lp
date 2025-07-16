b672bca7cfe64a76e56d8fc39b0211591ab99697
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-07-17 00:49:10 +0900

    お知らせ一覧ページちゃくしゅ

--- sabae-event-lp/src/components/pages/news/NewsListPage.tsx
+++ sabae-event-lp/src/components/pages/news/NewsListPage.tsx
@@ -1,21 +1,83 @@
-import { Link } from "react-router-dom";
-
-const dummyNews = [
-	{ id: "1", title: "お知らせその1" },
-	{ id: "2", title: "お知らせその2" },
-];
+import styled from "@emotion/styled";
+import { newsList } from "../../../consts/news";
+import {
+	S_DIV_CardContainer,
+	S_DIV_SectionWrapper,
+	S_SECTION_container,
+} from "../../../Style";
+import { ContentsTitle } from "../../contents/CotentsTitle";
+import { COLORS } from "../../../consts/color";
+import { TEXT_M, TEXT_S } from "../../../consts/typography";
 
 export const NewsListPage = () => {
 	return (
-		<div>
-			<h1>お知らせ一覧</h1>
-			<ul>
-				{dummyNews.map((news) => (
-					<li key={news.id}>
-						<Link to={`/news/${news.id}`}>{news.title}</Link>
-					</li>
-				))}
-			</ul>
-		</div>
+		<S_DIV_SectionWrapper>
+			<S_SECTION_container>
+				<ContentsTitle title="お知らせ" balloonList={[]} />
+				<DIV_CardContainer>
+					<UL_CardWrapper>
+						{newsList.map((news, index) => {
+							const isLink = news.url;
+							const LinkWrapper = isLink ? A_CardLink : DIV_CardLink;
+
+							return (
+								<LI_CardContainer key={index}>
+									<LinkWrapper {...(isLink ? { href: news.url } : {})}>
+										<DIV_Date>{news.date}</DIV_Date>
+										<P_Title>{news.title}</P_Title>
+									</LinkWrapper>
+								</LI_CardContainer>
+							);
+						})}
+					</UL_CardWrapper>
+				</DIV_CardContainer>
+			</S_SECTION_container>
+		</S_DIV_SectionWrapper>
 	);
 };
+
+const LI_CardContainer = styled.li`
+	padding: 0px;
+	display: block;
+	list-style: none;
+	border-bottom: 1px solid ${COLORS.BORDER};
+`;
+
+const UL_CardWrapper = styled.ul`
+	display: block;
+	margin-top: 0;
+	margin-bottom: 14px;
+`;
+
+const DIV_Date = styled.div`
+	${TEXT_S}
+	font-weight: bold;
+	color: ${COLORS.PRIMARY};
+`;
+
+const P_Title = styled.p`
+	${TEXT_M}
+`;
+
+const DIV_CardLink = styled.div`
+	display: block;
+	padding: 30px;
+`;
+
+// NOTE: withComponent('a') を使えば、タグを div → a に変えられる。
+const A_CardLink = styled(DIV_CardLink.withComponent("a"))`
+	&:hover ${DIV_Date}, &:hover ${P_Title} {
+		opacity: 0.5;
+	}
+
+	${P_Title} {
+		text-decoration: underline 1px;
+		text-underline-offset: 3px;
+	}
+`;
+
+const DIV_CardContainer = styled(S_DIV_CardContainer)`
+	// リストの上部パッディングを考慮
+	padding: 0px 30px 30px 30px;
+	min-height: 675px;
+`;
