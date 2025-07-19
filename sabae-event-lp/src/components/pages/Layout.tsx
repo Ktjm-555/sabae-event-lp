78f2780ca9c2fe4b16af1cc10c8260e4f88bb2b4
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-07-19 15:33:02 +0900

    余白の修正

--- sabae-event-lp/src/components/pages/Layout.tsx
+++ sabae-event-lp/src/components/pages/Layout.tsx
@@ -1,13 +1,26 @@
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 import { Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
 
 export const Layout = () => {
 	return (
-		<>
+		<StyledWrapper>
 			<Header />
-			<Outlet />
+			<StyledMain>
+				<Outlet />
+			</StyledMain>
 			<Footer />
-		</>
+		</StyledWrapper>
 	);
 };
+
+const StyledWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+`;
+
+const StyledMain = styled.main`
+	flex: 1;
+`;
