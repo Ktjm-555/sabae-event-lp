ea0a15e95e102b77aca85fc939784e9563a7ebc0
Ktjm-555 <102650893+Ktjm-555@users.noreply.github.com>
2025-07-19 14:21:54 +0900

    GA4見直し

--- sabae-event-lp/src/App.tsx
+++ sabae-event-lp/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
 import ReactGA from "react-ga4";
 import { Layout } from "./components/pages/Layout";
@@ -6,27 +6,54 @@ import TopPage from "./components/pages/TopPage";
 import { NewsListPage } from "./components/pages/news/NewsListPage";
 import { ScrollToHash } from "./components/ScrollToHash";
 import { NewsDetailPage } from "./components/pages/news/NewsDetailPage";
+import { useEffect } from "react";
 
 // NOTE：initializeは初期化+sendしている。
 // ルーティングを設定するときは二重送信に要注意！
 if (import.meta.env.MODE === "production") {
-	ReactGA.initialize("G-1P3N11YF8M");
+	ReactGA.initialize("G-1P3N11YF8M", {
+		gaOptions: {
+			send_page_view: false,
+		},
+	});
 }
+
+const usePageTracking = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		if (import.meta.env.MODE !== "production") return;
+		ReactGA.send({
+			hitType: "pageview",
+			page: location.pathname + location.search,
+		});
+	}, [location]);
+};
 
 function App() {
 	return (
 		<>
 			<BrowserRouter basename={import.meta.env.BASE_URL}>
 				<ScrollToHash />
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<TopPage />} />
-						<Route path="news" element={<NewsListPage />} />
-						<Route path="news/:newsId" element={<NewsDetailPage />} />
-					</Route>
-				</Routes>
+				<InnerApp />
 			</BrowserRouter>
 		</>
+	);
+}
+
+function InnerApp() {
+	console.log("InnerApp コンポーネント レンダリング");
+
+	usePageTracking();
+
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<TopPage />} />
+				<Route path="news" element={<NewsListPage />} />
+				<Route path="news/:newsId" element={<NewsDetailPage />} />
+			</Route>
+		</Routes>
 	);
 }
 
